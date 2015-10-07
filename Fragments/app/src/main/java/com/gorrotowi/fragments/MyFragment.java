a6b3643faa15d9532fc1b9b59010e496383319c4
Sebastian <sebastian.tellez.o@gmail.com>
2015-10-07 13:12:17 -0500

    add fragment static example

---
+++ Fragments/app/src/main/java/com/gorrotowi/fragments/MyFragment.java
@@ -0,0 +1,22 @@
+package com.gorrotowi.fragments;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by gorro on 07/10/15.
+ */
+public class MyFragment extends Fragment {
+
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_layout, container);
+        return rootView;
+    }
+}
